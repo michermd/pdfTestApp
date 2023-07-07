92bd1bae94482a57fc830078dfc70cf97b4cad3b
Daniel Micher <Daniel.Micher@pfizer.com>
2023-07-07 11:06:20 -0400

    Implemented PDF test app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { saveAs } from 'file-saver';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor() {}
+  fileToUpload: File | null = null;
+  fileUrl: string | null = null;
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    this.fileToUpload = file;
+    this.fileUrl = URL.createObjectURL(file);
+  }
+
+  saveFile() {
+    if (this.fileToUpload) {
+      const fileName = this.fileToUpload.name;
+      saveAs(this.fileToUpload, fileName);
+    }
+  }
+
+  getSafeUrl(url: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, NgxExtendedPdfViewerModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
